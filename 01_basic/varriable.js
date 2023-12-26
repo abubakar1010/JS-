655cc74a29450b54f8007e07a721324cb34d8f0e
abubakar1010 <154862242+abubakar1010@users.noreply.github.com>
2023-12-26 06:03:50 +0000

    container config done

---
+++ 01_basic/varriable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="abubakr@google.com"
+var accountPassword = "1234"
+accountCity ="Dhaka"
+
+// accountId = 2
+
+console.log(accountId);
+
+accountEmail = "abu@google.com"
+accountCity = "barisal"
+accountPassword = "38766727"
+console.table( [accountEmail, accountCity, accountPassword] )
