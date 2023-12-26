8d404b08d6bfb9db2cd23f8735ce0ec79f5b2642
abubakar1010 <154862242+abubakar1010@users.noreply.github.com>
2023-12-26 07:50:24 +0000

    Datatypes

---
+++ 01_basic/datatype.js
@@ -0,0 +1,26 @@
+"use strict";
+// alert( 3+ 3)
+console.log( 3+ 3)
+
+console.log("Abu bakar")
+
+// string => ""
+let name = "Abu Bkar"
+
+// number datatype, range- 2 to power 52 or 53
+// bigint when we have very big number
+let age = 18
+
+// bollean => true/false
+let isLoggedIn = false
+
+
+// Datatypes-
+// string => ""
+// number , range- 2 to power 52 or 53
+// bigint when we have very big number
+// bollean => true/false
+// null => standalone value
+// undefined => 
+// symbol => for uniqueness
+// object
